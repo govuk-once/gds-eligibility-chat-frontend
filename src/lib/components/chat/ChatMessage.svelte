7634d371a1ae1913c24fe661ac73162bfff5c7ac
edpackard-work <edward.packard@digital.cabinet-office.gov.uk>
2026-02-23 15:13:06 +0000

    fix css for headers

--- src/lib/components/chat/ChatMessage.svelte
+++ src/lib/components/chat/ChatMessage.svelte
@@ -1,122 +1,152 @@
 <script lang="ts">
-	import type { Message } from '$lib/types';
-	import StreamingText from '$lib/components/chat/StreamingText.svelte';
+    import type { Message } from '$lib/types';
+    import StreamingText from '$lib/components/chat/StreamingText.svelte';
 
-	let { message, isLast, loading, onUpdate } = $props<{
-		message: Message;
-		isLast: boolean;
-		loading: boolean;
-		onUpdate?: () => void;
-	}>();
+    let { message, isLast, loading, onUpdate } = $props<{
+        message: Message;
+        isLast: boolean;
+        loading: boolean;
+        onUpdate?: () => void;
+    }>();
 </script>
 
 <div class="message {message.role} {message.vault ? 'vault' : ''}">
-	<div class="message-content">
-		{#if message.html}
-			{#if message.role === 'assistant' && isLast && !loading && message.markdown}
-				<StreamingText messageId={message.id} content={message.markdown} stream={true} {onUpdate} />
-			{:else}
-				<!-- we have sanitised message.html with DOMPurify -->
-				<!-- eslint-disable-next-line svelte/no-at-html-tags -->
-				{@html message.html}
-			{/if}
-		{:else}
-			{message.text}
-		{/if}
-	</div>
+    <div class="message-content">
+        {#if message.html}
+            {#if message.role === 'assistant' && isLast && !loading && message.markdown}
+                <StreamingText messageId={message.id} content={message.markdown} stream={true} {onUpdate} />
+            {:else}
+                <!-- we have sanitised message.html with DOMPurify -->
+                <!-- eslint-disable-next-line svelte/no-at-html-tags -->
+                {@html message.html}
+            {/if}
+        {:else}
+            {message.text}
+        {/if}
+    </div>
 
-	{#if message.vault}
-		<div class="shield-icon">
-			<img
-				src="/icons/shield-check.svg"
-				alt="vaulted response"
-				aria-hidden="true"
-				class="privacy-icon"
-			/>
-		</div>
-	{/if}
+    {#if message.vault}
+        <div class="shield-icon">
+            <img
+                src="/icons/shield-check.svg"
+                alt="vaulted response"
+                aria-hidden="true"
+                class="privacy-icon"
+            />
+        </div>
+    {/if}
 </div>
 
 <style>
-	:global(.message a) {
-		color: black;
-	}
-	:global(.message p) {
-		margin: 0;
-		padding-left: 1em;
-		padding-right: 1em;
-	}
+    :global(.message a) {
+        color: black;
+    }
+    :global(.message p) {
+        margin: 0;
+        padding-left: 1em;
+        padding-right: 1em;
+    }
 
-	:global(.message ol ul h2 h3 h4 h5 h6) {
-		padding-left: 1em;
-		padding-right: 1em;
-	}
+    :global(.message ol) {
+        padding-left: 1em;
+        padding-right: 1em;
+    }
 
-	:global(.message h1) {
-		margin-top: 0;
-		padding-left: 0.5em;
-		padding-right: 0.5em;
-	}
+	  :global(.message ul) {
+        padding-left: 1em;
+        padding-right: 1em;
+    }
 
-	:global(.message li) {
-		padding-right: 1em;
-	}
-	:global(.message.assistant p + p) {
-		margin-top: 1.5em;
-	}
-	.message {
-		font-size: 1em;
-		overflow-wrap: break-word;
-		word-break: break-word;
-	}
+    :global(.message h1) {
+        margin-top: 0;
+        padding-left: 0.5em;
+        padding-right: 0.5em;
+    }
 
-	.message.user {
-		padding: 0.75em 1em;
-		border-radius: 10.86px;
-		margin-right: 1em;
-		margin-left: 4.5em;
-		text-align: left;
-		background-color: #d9d9d9;
-		align-self: flex-end;
-		max-width: 100%;
-		display: flex;
-		gap: 0.5em;
-		align-items: center;
-	}
+    :global(.message h2) {
+        padding-left: 0.5em;
+        padding-right: 0.5em;
+    }
 
-	.message.user.vault {
-		padding: 0.75em 1em 0.75em 1.5em;
-		gap: 0.5em;
-	}
+	:global(.message h3) {
+        padding-left: 0.5em;
+        padding-right: 0.5em;
+    }
 
-	.message.assistant,
-	.message.error {
-		padding-left: 0;
-		border-radius: 0;
-		background-color: transparent;
-		text-align: left;
-		align-self: stretch;
-	}
-	.message.error {
-		color: #c00;
-	}
+	:global(.message h4) {
+        padding-left: 0.5em;
+        padding-right: 0.5em;
+    }
 
-	.actions {
-		margin-top: 1em;
-		display: flex;
-		flex-direction: column;
-		gap: 0.5em;
-	}
-	.shield-icon {
-		display: flex;
-		align-items: flex-end; /* bottom */
-		justify-content: flex-end; /* right */
-		align-self: flex-end;
-	}
+	:global(.message h5) {
+        padding-left: 0.5em;
+        padding-right: 0.5em;
+    }
 
-	.privacy-icon {
-		width: 1em;
-		height: 1em;
-		padding-bottom: 0.1em;
-	}
+	:global(.message h6) {
+        padding-left: 0.5em;
+        padding-right: 0.5em;
+    }
+
+    :global(.message li) {
+        padding-right: 1em;
+    }
+    :global(.message.assistant p + p) {
+        margin-top: 1.5em;
+    }
+    .message {
+        font-size: 1em;
+        overflow-wrap: break-word;
+        word-break: break-word;
+    }
+
+    .message.user {
+        padding: 0.75em 1em;
+        border-radius: 10.86px;
+        margin-right: 1em;
+        margin-left: 4.5em;
+        text-align: left;
+        background-color: #d9d9d9;
+        align-self: flex-end;
+        max-width: 100%;
+        display: flex;
+        gap: 0.5em;
+        align-items: center;
+    }
+
+    .message.user.vault {
+        padding: 0.75em 1em 0.75em 1.5em;
+        gap: 0.5em;
+    }
+
+    .message.assistant,
+    .message.error {
+        padding-left: 0;
+        border-radius: 0;
+        background-color: transparent;
+        text-align: left;
+        align-self: stretch;
+    }
+    .message.error {
+        color: #c00;
+    }
+
+    .actions {
+        margin-top: 1em;
+        display: flex;
+        flex-direction: column;
+        gap: 0.5em;
+    }
+    .shield-icon {
+        display: flex;
+        align-items: flex-end; /* bottom */
+        justify-content: flex-end; /* right */
+        align-self: flex-end;
+    }
+
+    .privacy-icon {
+        width: 1em;
+        height: 1em;
+        padding-bottom: 0.1em;
+    }
 </style>
