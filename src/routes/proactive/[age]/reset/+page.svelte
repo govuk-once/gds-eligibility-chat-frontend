2faa10ade05ced58d9951afb47d7cd53ff4ed16d
edpackard-work <edward.packard@digital.cabinet-office.gov.uk>
2026-02-24 09:37:02 +0000

    add proactive reset urls

---
+++ src/routes/proactive/[age]/reset/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
+	import { page } from '$app/state';
+	import { initializeChat } from '$lib/chat.svelte';
+
+	onMount(() => {
+		const age = page.params.age;
+		localStorage.clear();
+		initializeChat();
+		if (age) {
+			goto(`proactive/${age}`);
+		} else {
+			goto('/');
+		}
+	});
+</script>
